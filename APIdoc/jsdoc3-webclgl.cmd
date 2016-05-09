6e1b58ccefdf7d61bd3bacacc359256d0f33affd
3droberto <3droberto@gmail.com>
2016-05-09 19:23:05 +0200

    API updated

--- APIdoc/jsdoc3-webclgl.cmd
+++ APIdoc/jsdoc3-webclgl.cmd
@@ -8,7 +8,7 @@ set outputDir=APIdoc
 
 cd /D %jsdocdir%
 echo GENERATING API DOC
-jsdoc -t templates/docstrap-master/template -c confWebCLGL.json -d ..\%outputDir% %sourcesDir%WebCLGLUtils.class.js %sourcesDir%WebCLGLBuffer.class.js %sourcesDir%WebCLGLBufferItem.class.js %sourcesDir%WebCLGLKernel.class.js %sourcesDir%WebCLGLKernelProgram.class.js %sourcesDir%WebCLGLVertexFragmentProgram.class.js %sourcesDir%WebCLGLWork.class.js %sourcesDir%WebCLGL.class.js
+jsdoc -t templates/docstrap-master/template -c confWebCLGL.json -d ..\%outputDir% %sourcesDir%WebCLGLUtils.class.js %sourcesDir%WebCLGLBuffer.class.js %sourcesDir%WebCLGLBufferItem.class.js %sourcesDir%WebCLGLKernel.class.js %sourcesDir%WebCLGLVertexFragmentProgram.class.js %sourcesDir%WebCLGLWork.class.js %sourcesDir%WebCLGL.class.js
 
 
 echo.
